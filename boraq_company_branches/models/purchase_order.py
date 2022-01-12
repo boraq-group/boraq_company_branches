489ee452d89d11a1fd62b380a6d8d06ff7870ee2
Imbar Budiman <imbarbudiman@yahoo.com>
2022-01-12 23:19:53 +0700

    [REF] boraq_company_branches: Replace action_view_invoice() with  _prepare_invoice()

--- boraq_company_branches/models/purchase_order.py
+++ boraq_company_branches/models/purchase_order.py
@@ -7,7 +7,11 @@ class PurchaseOrder(models.Model):
     branch_id = fields.Many2one('company.branches', string='Branch', domain="[('company_id','=',company_id)]")
     
     
-    def action_view_invoice(self):
-        result = super(PurchaseOrder,self).action_view_invoice()
-        result['context'].update({'default_branch_id':self.branch_id.id}) 
-        return result
+    def _prepare_invoice(self):
+        """ Override """
+        invoice_vals = super(PurchaseOrder, self)._prepare_invoice()
+        invoice_vals.update({
+            'branch_id': self.branch_id
+        })
+        return invoice_vals
+
